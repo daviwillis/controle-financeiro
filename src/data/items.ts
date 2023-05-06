66ffca3c7ef50651db52eff4613eeaf24b0624d3
Davi Nascimento <daviwillis@protonmail.com>
2023-05-06 16:57:45 -0300

    feat:moving-to-git

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+  { date: new Date(2022, 8, 1), category: 'food', title: 'McDonalds', value: 32.12 },
+  { date: new Date(2022, 8, 2), category: 'salary', title: 'Salário', value: 3600 },
+  { date: new Date(2022, 8, 3), category: 'food', title: 'Burger King', value: 28 },
+  { date: new Date(2022, 8, 1), category: 'rent', title: 'Aluguel', value: 2300 },
+];
